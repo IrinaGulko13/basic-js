690a56cd98965d2add5efec71e2056ffa4ea9bf6
Iryna Hulko <irinagulko13@gmail.com>
2020-02-27 17:35:45 +0300

    feat:transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,25 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+    let newArray = [];
+    if (!Array.isArray(arr)) {
+        throw new Error();
+    } else if (arr.length === 0) {
+        return arr;
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '--discard-next') {
+            i++;
+        } else if (arr[i] === '--discard-prev') {
+                newArray.pop(); 
+        } else if (arr[i] === '--double-next') {
+                if (i + 1 !== arr.length) {
+                newArray.push(arr[i + 1]);}
+        }else if (arr[i] === '--double-prev') {
+                if (i > 0) {
+                newArray.push(arr[i - 1]); }
+        } else {
+            newArray.push(arr[i]);
+                    }
+                }
+    return newArray;
+}
+
