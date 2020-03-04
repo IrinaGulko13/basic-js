f643039b1fd24134b97a8056dce28e1439d4393c
Iryna Hulko <irinagulko13@gmail.com>
2020-03-04 21:49:16 +0300

    feat:solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,24 +1,73 @@
 class VigenereCipheringMachine {
-    constructor (val){
-        this.val = val;
+    constructor (reverse){
+        this.reverse = reverse;
     }
-    encrypt (message, key) {
+    encrypt = (message, key) => {
         if(!message||!key){
             throw new Error();
     }
     let mes = message.toUpperCase();
+    let mesArr = mes.split('');
     let keyword = key.toUpperCase();
-    let result = '';
+    let keyArr = keyword.split('');
+    let alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    keyArr = keyArr.map((val)=>alphabet.findIndex((v)=>v === val));
+    let keyInd = 0;
+    const newWord = mesArr.map((val, i)=>{
+    const letInd =alphabet.findIndex((v)=>v === val);
+    if (keyInd >= keyArr.length) {
+        keyInd = 0;
+    }
+    if (letInd != -1) {
+        let newInd = letInd + keyArr[keyInd];
+        keyInd=keyInd+1;
+        if (newInd >= alphabet.length) {
+            newInd = newInd % alphabet.length;
+        }
+        return alphabet[newInd];
+    }
+    return val;
+    });
+    let word = newWord.join('')
+    if (this.isReverse) {
+        return word.reverse();
+    }
+        return word;
+   }
+   
     
+   decrypt = (message, key) => {
+    if(!message||!key){
+        throw new Error();
 }
 
-    decrypt (message,key) {
-        if(!message||!key){
-            throw new Error();
-    }
     let mes = message.toUpperCase();
+    let mesArr = mes.split('');
     let keyword = key.toUpperCase();
-    let result = '';
-}
-}
+    let keyArr = keyword.split('');
+    let alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    keyArr = keyArr.map((val)=>alphabet.findIndex((v)=>v === val));
+    let keyInd = 0;
+    const newWord = mesArr.map((val, i)=>{
+    const letInd =alphabet.findIndex((v)=>v === val);
+    if (keyInd >= keyArr.length) {
+        keyInd = 0;
+    }
+    if (letInd != -1) {
+        let newInd = Math.abs(letInd + alphabet.length - keyArr[keyInd]);
+        keyInd=keyInd+1;
+        if (newInd >= alphabet.length) {
+            newInd = newInd % alphabet.length;
+        }
+        return alphabet[newInd];
+    }
+    return val;
+    });
+    let word = newWord.join('')
+    if (this.isReverse) {
+        return word.reverse();
+    }
+     return word;
+   }
+} 
 module.exports = VigenereCipheringMachine;
